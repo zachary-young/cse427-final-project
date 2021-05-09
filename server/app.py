67227bd4ac901a289400b6f9002d42f51b49a3b4
Jordan Stone <jordans2299@gmail.com>
2021-05-09 07:54:38 -0500

    upload and download files

--- server/app.py
+++ server/app.py
@@ -1,9 +1,34 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, send_file, session
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+import os
+
+# from werkzeug import secure_filename
+app = Flask(__name__, template_folder='../client/template', static_folder='../client/static')
+app.secret_key = '\x17Y\x98\x10!\x9c"\x11\t\x14\x03v\xa1\x1c\xc2\x07|\xdd\xc2H\xde\x9f\x9b\x19'
+uploads_dir = os.path.join(app.instance_path, 'files')
+
 
 @app.route('/')
-def hello_world():
-    return 'It works!'
+def home():
+    return render_template("index.html")
+    # return 'It works!'
+    
+#stores uploaded file
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['myFile']
+        session["fileName"] = secure_filename(f.filename)
+        f.save(os.path.join(uploads_dir, secure_filename(f.filename)))
+        return render_template("index.html")
+
+#sends file back to user
+@app.route('/downloader')
+def download_file():
+    return send_file("instance/files/"+session["fileName"])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
+
+
