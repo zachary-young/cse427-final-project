e8027e36c0229ab97efd581f6dbadb29d5f99a1c
Zachary Young <zachary.young@wustl.edu>
2021-05-09 17:48:56 -0400

    Initial sound processing

--- server/app.py
+++ server/app.py
@@ -1,33 +1,25 @@
-from flask import Flask, render_template, request, send_file, session
+from flask import Flask, request, send_file, jsonify
 from werkzeug.utils import secure_filename
-from werkzeug.datastructures import  FileStorage
+from flask_cors import CORS
+from scipy.io import wavfile
 import os
 
-app = Flask(__name__, template_folder='../client/template', static_folder='../client/static')
-app.secret_key = '\x17Y\x98\x10!\x9c"\x11\t\x14\x03v\xa1\x1c\xc2\x07|\xdd\xc2H\xde\x9f\x9b\x19'
-uploads_dir = os.path.join(app.instance_path, 'files')
+app = Flask(__name__)
+CORS(app)
 
+UPLOAD_FOLDER = os.path.join(app.instance_path, 'files')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def home():
-    return render_template("index.html")
+    f = request.files["audio"]
+    filename = secure_filename(f.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    f.save(filepath)
+    samplerate, data = wavfile.read(filepath)
+    return jsonify({"data": data.tolist()})
     
-#stores uploaded file
-@app.route('/uploader', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        f = request.files['myFile']
-        session["fileName"] = secure_filename(f.filename)
-        print(uploads_dir)
-        f.save(os.path.join(uploads_dir, secure_filename(f.filename)))
-        return render_template("index.html")
-
-#sends file back to user
-@app.route('/downloader')
-def download_file():
-    return send_file("instance/files/"+session["fileName"])
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
 
 
